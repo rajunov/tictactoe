18318f73dbedf3551c01e96afd5bc22a1bcb83ae
Micah Rajunov <rajunov@gmail.com>
2014-04-25 18:08:45 -0700

    Algorithm is functional. Some UI polish needed. I don't understand what is going on all the time so I might revise the algo.

--- README.md
+++ README.md
@@ -1,15 +1,66 @@
 tic tac toe
 =========
+This is the stuff coding challenges are made of.
 
-This is the stuff coding challenges are made of
+### What?
+Javascript-based tic tac toe game.
 
-#### What?
-Javascript-based tic tac toe game. Stuck to the basics to demonstrate fundamental coding ability and UI design.
+Given my target user is someone whose goal is to evaluate my coding competency, I stuck to the basics to demonstrate fundamental coding ability in building a robust UI via the front-end. My methods were to complete the coding challenge in the minimum time possible while closely mimicking the process, tools, and abilities I'd use in my day-to-day work.
 
-#### Process
-Before googling anything (except for syntax lookups), I built the entire structure sans algorithm, as a testament to what I know off the bat.
+#### Where
+Play with the finished product here: http://rajunov.com/tictactoe/index.html
 
-#### Other Stuff
-- Time to completion: X hours.
-- Not tested in IE or other non-browsers. 
-- Not meant to be gorgeous or pixel-perfect.
+#### Screenshots
+If you're seriously too lazy to click the link above, here's what it looks like.
+
+
+
+### Process
+Since this is a coding challenge, I assume you're more interested in knowing _how_ I got here than the fact that I got here at all, so here's the play-by-play.
+
+Moreover, I'm a big fan of documenting the process, since it gives transparency to why decisions were made, what options have already been considered, and how we arrived at the current implementation, thus avoiding duplication of work down the line. It's even more important when this is all we have to communicate with each other. Hence the overkill.
+
+
+##### Set up
+Before googling anything (except for syntax lookups), I built a basic UI structure sans algorithm, as a testament to what I know off the bat.
+
+##### Research
+Then I looked up a few ticky tacky implementations. Minimax seems to be the algorithm of choice. Or rather, Negamax. [This guy](http://www.neverstopbuilding.com/minimax) has an already impressive [web app](http://perfecttictactoe.herokuapp.com/), and an even more impressive explanation. You should probably hire him, I promise mine is not as stellar.
+
+His implementation supposedly comes with an API. Hooking into APIs sums up most of my job, so I wouldn't have considered this "cheating" but rather, being clever. Unfortunately the API is not exposed (but I got to learn about this neat error: ```XMLHttpRequest cannot load http://bla. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.```) So short of cloning that repo and setting it up on my own server, I had to bite the bullet, put on my college hat, and write an actual algorithm.
+
+##### Gettin' my hands dirty
+I laid out my "MVC" to keep track of data and game state, until everything except the actual solver were ready. Ran into some sneaky JS reference issues, which were taken care of with some deep copying using ```$.extend()``` and creating new instances of objects.
+
+##### Reverse engineering
+It was useful to follow along with the aforementioned implementation and [this JS version](http://mkuklis.github.io/tictactoe/docs/tictactoe.html), as well as the [programming bible](http://en.wikipedia.org/wiki/Negamax), and other articles for reference.
+
+I coded first, then tried to understand what the algorithm was doing as I tested and debugged. At this point, it works, and conceptually I get what is going on, but I couldn't really explain to you what the code is doing 100% of the time.
+
+
+### Limitations and Considerations
+
+This is definitely not the best implementation of tic tac toe out there. It's not meant to be. You can always optimize further. To avoid scope creep, I aimed for a presentable MVP. Yes, you always go first. Yes, you are always 'o'. Yes, the UI is not exactly gorgeous.
+
+Since I'm usually working within an existing framework like Angular, it's been a long time since I used raw OO JS classes. I had to [brush up](http://yehudakatz.com/2011/08/12/understanding-prototypes-in-javascript/), but honestly they've always been little confusing to me. I realize my code is a mixed bag.
+
+
+### Other Stuff
+- Time to completion: 8 hours, so far. _IN PROGRESS_
+- Not tested in IE or other non-browsers.
+- Versioned as a (public) submodule off of my main (private) repo so my commit-based progress can be scrutinized if so desired. Please feel free to comment.
+
+
+### Closing Remarks
+
+My primary focus as a developer is on UI. In 5+ years of mostly front-end coding, the only time I've ever had the need to implement anything remotely resembling an algorithm, I thankfully realized all I needed was [_.shuffle()](http://underscorejs.org/#shuffle) before diving into my own custom Fisher-Yates. So this exercise was a change of pace for me.
+
+Hopefully this shows you that I can code up a decent web app. I also offer a range of product-building services on top of programming, such as design, snark, and fun.
+
+
+
+
+
+
+
+
--- ttt.js
+++ ttt.js
@@ -1,70 +1,73 @@
-  
+
 // "Model"
-var Board = function() {
-  this.board = [0,0,0,0,0,0,0,0,0]
+var Board = function(config_board) {
+  this.board = [0,0,0,0,0,0,0,0,0];
+
   this.player = 'o'
   this.opponent = 'x'
-  this.win_sequences = [
-      [0,1,2],[3,4,5],[6,7,8], // rows
-      [0,3,6],[1,4,7],[2,5,8], // cols
-      [0,4,8],[2,4,6] // diagonals
-    ]
-  this.winning_sequence = [];
-  this.winner = null
+
+  // Option to start the board with preset configuration
+  // Useful for sub-board sequence calculation
+  if( _.isArray(config_board) )
+    $.extend(true, this.board, config_board) // lose the reference
 }
 
 Board.prototype = {
 
+  win_sequences : [
+      [0,1,2],[3,4,5],[6,7,8], // rows
+      [0,3,6],[1,4,7],[2,5,8], // cols
+      [0,4,8],[2,4,6] // diagonals
+  ],
   resetBoard : function() {
     this.board = [0,0,0,0,0,0,0,0,0]
-    this.winning_sequence = [];
-    this.winner = null
+  },
+  // Player-agnostic function, returns opposite player
+  otherPlayer : function(who) {
+    if( who == this.player )
+      return this.opponent
+    else if( who == this.opponent )
+      return this.player
+    else
+      console.log('cannot find otherPlayer of "'+who+'". broken code?')
   },
   isFull : function() {
     return _.indexOf(this.board, 0) == -1
   },
   // Returns winning player
-  findWinner : function() {
-    if( this.win(this.player) )
+  getWinner : function() {
+    if( this.findWinningSequence(this.player) )
       return this.player
-    else if( this.win(this.opponent) )
+    else if( this.findWinningSequence(this.opponent) )
       return this.opponent
     else
       return false
   },
-  hasWinner : function() {
-    return this.winner
-  },
   // Returns winning sequence if "who" is a winner, false otherwise
   // Stores winning sequence and winner
-  win : function(who) {
+  findWinningSequence : function(who) {
 
     // use jQuery's .each since it allows breaks
     var win_seq = false
     var that = this
-    $.each(this.win_sequences, function(i, seq){
+    _.each(this.win_sequences, function(seq, i){
       if( that._matchThree(seq, who) ) {
         win_seq = seq
         return false // break
       }
     })
 
-    // Store in model
-    if(win_seq) { 
-      this.winning_sequence = win_seq
-      this.winner = who
-    }
     return win_seq
   },
   tie : function() {
     return this.isFull() && !this.winner
   },
   // Marks a move on the board
-  move : function(pos, player) {
-    if( pos < 0 || pos > 8 ) 
-      console.log('this should not happen. micah made a boo boo')  
+  move : function(pos, who) {
+    if( pos === undefined || pos === null || pos < 0 || pos > 8 )
+      console.log('pos='+pos+'. this should not happen. micah made a boo boo')
     else
-      this.board[pos] = player
+      this.board[pos] = who
   },
   // Returns indices of empty spaces in board
   getEmptySpaces : function() {
@@ -78,8 +81,8 @@ Board.prototype = {
   // Checks whether board contains player marker "who" in all 3 positions
   // _yeah I know, it's not really private
   _matchThree : function(sequence, who) {
-    if( this.board[sequence[0]] == who && 
-        this.board[sequence[1]] == who && 
+    if( this.board[sequence[0]] == who &&
+        this.board[sequence[1]] == who &&
         this.board[sequence[2]] == who )
       return true
   }
@@ -89,49 +92,77 @@ Board.prototype = {
 
 var Negamax = function() {
   this.MAX_DEPTH = 6
-  this.MAX_VALUE = 1000
-  this.MIN_VALUE = (-1 * this.MAX_VALUE)
+
+  this.MAX_VALUE = 10
+  this.MIN_VALUE = -this.MAX_VALUE
 }
 
 Negamax.prototype = {
-  
-  // Uses negamax, a variation of minimax that assumes 
+
+  // Uses negamax, a variation of minimax that assumes
   //   a zero-sum game (the losses of one player equal the gains of another)
+
+  //   TODO: not really alpha-beta pruning here... just kind-of-alpha stuff
   //   w/ alpha-beta pruning, where is breaks from diving into the branch
   //   if it realizes the current move cannot be better than a previous one
+
   // Called recursively
   // The meat between the bread
   _solve : function(board, player, depth) {
-    
-    console.log('in _solve, player='+player+', depth='+depth)
-    
+
     // Check ending conditions
     if( depth > this.MAX_DEPTH )
       return 0
 
-    var winner = board.findWinner()
+    // Returns score if there is a winner
+    var winner = board.getWinner()
+    var opponent = board.otherPlayer(player)
     if( winner == player )
       return this.MAX_VALUE
-    else if( winner && winner != player )
+    else if( winner == opponent )
       return this.MIN_VALUE
     else if( board.isFull() )
       return 0
 
-    var alpha = this.MIN_VALUE
     var possible_moves = board.getEmptySpaces()
+    var best_scores = []
+    var alpha = this.MIN_VALUE
+    var that = this
 
-    // Testing, return first of possible moves
-    this.next_move = _.first(_.shuffle(possible_moves))
-    return this.next_move
+    // use jQuery's $.each since it allows break
+    $.each(possible_moves, function(i, position){
+      // Copy current board and make this move
+      var subboard = new Board(board.board)
+      subboard.move(position, player)
+
+      // Traverse all possible child boards recursively
+      var score = -1 * that._solve(subboard, opponent, depth+1)
+      if( alpha < score )
+        alpha = score
+
+      // Push the currently calculated score for this possible move
+      if( depth === 0 )
+        best_scores.push({ 'position': position, 'score': score })
+    })
+
+    // At the root node, pick the next_move with the highest score
+    if( depth === 0 ) {
+      var considered_moves = _.filter(best_scores,
+                                function(s){ return s.score === alpha })
+
+      // Add an element of randomness in case there are many "best" moves
+      var rand = _.sample(considered_moves)
+      this.next_move = rand.position
+    }
+
+    return alpha
   },
 
   // Public function to call
   // Returns position id of next move
   nextMove : function(board, player) {
     this.next_move = -1
-    this._solve(board, player, 0)
-    
-    console.log('the next move is: '+this.next_move)
+    var score = this._solve(board, player, 0)
     return this.next_move
   }
 };
@@ -139,65 +170,70 @@ Negamax.prototype = {
 
 
 
-// Angular would've been overkill here... 
+// Angular would've been overkill here...
 // showing off jQuery Skillz instead
 $(function() {
 
-  // Init new board and algo instance
-  var board = new Board()
-  var algorithm = new Negamax(board.board, board.player)
+  // Init new game and algo instance
+  var game = new Board()
+  var algorithm = new Negamax(game.board, game.player)
+  var game_ended = false
 
   // Player "plays" click handler
   $('.board td').on('click', function(){
-    if( board.hasWinner() ||
-       $(this).hasClass(board.player) || $(this).hasClass(board.opponent) )
+    if( game_ended ||
+        $(this).hasClass(game.player) || $(this).hasClass(game.opponent) )
       return
 
-    makeMove($(this).data('position'), board.player)
+    makeMove($(this).data('position'), game.player)
   })
 
   // Reset click handler
   $('.reset').on('click', function(){
-    $('.board td').removeClass(board.player).removeClass(board.opponent)
+    $('.board td').removeClass(game.player).removeClass(game.opponent)
                   .removeClass('win')
     $('.alert').hide()
 
-    board.resetBoard()
+    game_ended = false
+    game.resetBoard()
   })
 
   // Records move on game board and calls computer to make next move
   function makeMove(position, who) {
 
     $('.board td:eq('+position+')').addClass(who)
-    board.move(position, who)
+    game.move(position, who)
 
     // Are we done?
-    var winner = board.findWinner()
-    if( board.isFull() || winner )
+    var winner = game.getWinner()
+    if( game.isFull() || winner )
       return endGame(winner)
 
     // Not done, make computer move
-    else if( who == board.player ) {
-      var position = algorithm.nextMove(board, board.player)
+    else if( who == game.player ) {
+      var calculate_game = new Board(game.board)
+      var position = algorithm.nextMove(calculate_game, game.opponent)
       // already recursive, and this isn't even the real algorithm!
-      makeMove(position, board.opponent)
+      makeMove(position, game.opponent)
     }
   }
 
   // Helper UI function to display end game message
   function endGame(winner) {
-    if( winner == board.player )
+    game_ended = true
+
+    if( winner == game.player )
       $('.alert-success').show()
-    else if( winner == board.opponent )
+    else if( winner == game.opponent )
       $('.alert-error').show()
     // this 'if' is extraneous, but good for error checks
-    else if( board.tie() ) 
+    else if( game.tie() )
       $('.alert-info').show()
 
-    highlightWin(board.winning_sequence)
+    highlightWin( game.findWinningSequence(winner) )
   }
 
-  // Helper UI function to highlight the winning sequence 
+  // Helper UI function to highlight the winning sequence
   function highlightWin(sequence) {
 
     var $td = $('.board td')
